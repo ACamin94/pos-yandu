a21e76f9999374706100658f7ccd584ab210b87d
Achmad Khoirul Amin <158138877+ACamin94@users.noreply.github.com>
2024-01-31 15:05:04 +0700

    index.php

--- index.php
+++ index.php
@@ -7,30 +7,30 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="assets/img/favicon/logo-01.png"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="/assets/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/css/util.css">
-	<link rel="stylesheet" type="text/css" href="/posyandu/assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('/posyandu/assets/images/bg-01.jpg');">
+		<div class="container-login100" style="background-image: url('/assets/images/bg-01.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Login E-Posyandu
@@ -86,21 +86,21 @@
 	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="/assets/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/animsition/js/animsition.min.js"></script>
+	<script src="/assets/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/bootstrap/js/popper.js"></script>
-	<script src="/posyandu/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/select2/select2.min.js"></script>
+	<script src="/assets/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/daterangepicker/moment.min.js"></script>
-	<script src="/posyandu/assets/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="/assets/vendor/daterangepicker/moment.min.js"></script>
+	<script src="/assets/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/vendor/countdowntime/countdowntime.js"></script>
+	<script src="/assets/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-	<script src="/posyandu/assets/js/main.js"></script>
+	<script src="/assets/js/main.js"></script>
 
 </body>
 </html>
