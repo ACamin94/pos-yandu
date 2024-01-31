5fd55ae798aa6ecde2bd19d1d67f06c3fafc7b8f
Achmad Khoirul Amin <158138877+ACamin94@users.noreply.github.com>
2024-01-31 15:01:38 +0700

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="/posyandu/assets/img/favicon/logo-01.png"/>
+	<link rel="icon" type="image/png" href="assets/img/favicon/logo-01.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/posyandu/assets/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
